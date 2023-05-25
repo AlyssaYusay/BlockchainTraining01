05ec146f2c4653746f7ff4602af6d4b4d3920f29
Alyssa Jagonob <alysjag@gmail.com>
2023-05-25 08:15:03 +0800

    added functions to NFT-Collection/client/src/App.js

--- Nft-Collection/client/src/App.js
+++ Nft-Collection/client/src/App.js
@@ -65,6 +65,103 @@ function App() {
     }
   };
 
+  //EOA && CA
+  //EOA - Metamask (private key)
+  //CA - (NO private key)
+
+  const connectWallet = async () => {
+    try {
+      // await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // Function to start Presale
+  // During preSale, discounted prices, time is limited, exclusivity
+  // These are for those who whitelisted
+  const startPresale = async () => {
+    try {
+      // const signer = await getProviderOrSigner(true);
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        abi
+        // signer
+      );
+
+      const tx = await nftContract.startPresale();
+      setLoading(true);
+      await tx.wait();
+      //can add more implementation
+      setLoading(false);
+
+      window.alert("Presale has started!");
+      // await checkIfPresaleStarted();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const checkIfPresaleStarted = async () => {
+    // can be imporoved??
+    try {
+      // const provider = await getProviderOrSigner();
+
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        abi
+        // provider
+      );
+
+      const _presaleStarted = await nftContract.presaleStarted();
+
+      setLoading(true);
+      await _presaleStarted.wait();
+      setLoading(false);
+
+      //if it is false get the owner
+      if (!_presaleStarted) {
+        // await getOwner();
+      }
+      setPresaleStarted(_presaleStarted);
+      return _presaleStarted;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  // check if presaleEnded
+  const checkIfPresaleEnded = async () => {
+    try {
+      // const provider = await getProviderOrSigner();
+
+      const nftContract = new Contract(
+        NFT_CONTRACT_ADDRESS,
+        abi
+        // provider
+      );
+
+      const _preSaleEnded = await nftContract.presaleEnded();
+
+      const hasEnded = Math.floor(Date.now) / 1000 >= presaleEnded;
+      //will throw true or false (if else)
+      // const hasEnded = _preSaleEnded.lt(Math.floor(Date.now) / 1000);
+
+      if (hasEnded) {
+        setPresaleEnded(true);
+      } else {
+        setPresaleEnded(false);
+      }
+
+      return hasEnded;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  //function to get Owner
+
   return <div className="App"></div>;
 }
 
